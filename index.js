b87615a8b5cc4440c74731d12f0c92a61aa7b3da
Monica Tamaru Jones <mntamaru@gmail.com>
2019-03-13 23:58:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const monologueLines = [
 
 const totalBatteries = batteryBatches.reduce((acc, cv) => acc + cv, 0);
 
-const wordCountMap = monologueLines.reduce(function(line, words) {
-  let wordNum = line.split(" ").length
+const wordCountMap = monologueLines.reduce(function(mono, line) {
+  let wordNum = line.split(" ").length;
+  
+  
 })
